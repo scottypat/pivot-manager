431054343c0e49abbb508ae4851f69288b11d5c5
Scott <scotty.pat@gmail.com>
2017-04-27 11:42:53 -0600

    Updated storeMoistureData to handle json data

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 import web
+import json
 
-db = web.database(dbn='sqlite', host='localhost', db='pivotmanager')
+db = web.database(dbn='sqlite', db='pivotmanager.db')
 
-def storeMoistureData(stkData, sampleTime):
-    db.insert("dataLogs", dataType="moisture", dataObj=stkData, dateTime=sampleTime)
+def storeMoistureData(strData, sampleTime):
+    jsonData = json.loads(strData)    
+    db.insert("moisture", sensorId=jsonData["sensorId"], depth1=jsonData["depth1"], depth2=jsonData["depth2"], depth3=jsonData["depth3"], depth4=jsonData["depth4"], vSys=jsonData["vSys"], soilTemp=jsonData["soilTemp"], dataObj=strData, dateTime=sampleTime)
     
 def getMoistureUploadData():
     return db.select("dataLogs", where="uploded = 0 AND dataType='moisture'")
