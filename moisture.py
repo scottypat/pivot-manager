5ed4db6100474cf281711290f858e188989c5c7e
Scott <scotty.pat@gmail.com>
2017-04-27 11:10:42 -0600

    Converted the getData function to use Json instead of comma del text

--- moisture.py
+++ moisture.py
@@ -1,29 +1,38 @@
 import wiringpi
 import httplib
 import json
-
-strMoistureData = ""
-stackMoistureData = []
+import string
 
 def setupLora():
     wiringpi.wiringPiSetup()
     loraId = wiringpi.serialOpen("/dev/serial0",9600)
     return  loraId
 
-def getData(loraId):
+def getData(loraId, loggerId):
+    stackMoistureData = []
+    bMoistureData = False
+    
     while wiringpi.serialDataAvail(loraId) > 0:
         intDataChar = wiringpi.serialGetchar(loraId)
         strDataChar = chr(intDataChar)
         
         if strDataChar == "$":
             bMoistureData = True
+            strMoistureData = ""
         
         if bMoistureData == True:
-            strMoistureData += strDataChar
+            strMoistureData =  strMoistureData + strDataChar
         
-        if strDataChar == "!":
-            bMoistureData = False
-            stackMoistureData.append(strMoistureData)
+        if strDataChar == "!" and bMoistureData == True:
+            # Trim start and end characters, and load json data
+            strMoistureData = strMoistureData[2:-2]            
+            jsonData = json.loads(strMoistureData)
+            
+            #Check if data is intended for this data logger            
+            if jsonData["loggerId"] == loggerId:   
+                stackMoistureData.append(strMoistureData)                       
+            
+            bMoistureData = False                
             strMoistureData = ""
             
     return stackMoistureData
