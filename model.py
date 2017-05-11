c1c03fef897df3e23b28bd4cd26da1ad23335fe0
Scott <scotty.pat@gmail.com>
2017-05-10 20:18:33 -0600

    Added error handling for db insert of moisture data

--- model.py
+++ model.py
@@ -5,7 +5,13 @@ db = web.database(dbn='sqlite', db='pivotmanager.db')
 
 def storeMoistureData(strData, sampleTime):
     jsonData = json.loads(strData)    
-    db.insert("moisture", sensorId=jsonData["sensorId"], depth1=jsonData["depth1"], depth2=jsonData["depth2"], depth3=jsonData["depth3"], depth4=jsonData["depth4"], vSys=jsonData["vSys"], soilTemp=jsonData["soilTemp"], dataObj=strData, dateTime=sampleTime)
+    
+    try:
+        db.insert("moisture", sensorId=jsonData["sensorId"], depth1=jsonData["depth1"], depth2=jsonData["depth2"], depth3=jsonData["depth3"], depth4=jsonData["depth4"], vSys=jsonData["vSys"], soilTemp=jsonData["soilTemp"], dataObj=strData, dateTime=sampleTime)
+    
+    except:
+        error = "Error Inserting Moisture Data: " + jsonData
+        print error
     
 def getMoistureUploadData():
     return db.select("dataLogs", where="uploded = 0 AND dataType='moisture'")
