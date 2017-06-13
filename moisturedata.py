a4309c209f8c327c406bc54e84df2d24629dc458
Scott <scotty.pat@gmail.com>
2017-06-13 11:50:50 -0600

    Added script to select all moisture data form the db.

---
+++ moisturedata.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+def select_all(conn, table):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM " + table)
+ 
+    rows = cur.fetchall()
+ 
+    for row in rows:
+        print(row)
+        
+def main():
+    database = "pivotmanager.db"
+ 
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        print("Query all moisture data:")
+        select_all_tasks(conn, "moisture) 
+ 
+if __name__ == '__main__':
+    main()
