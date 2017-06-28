014373cb63deb98e06d0aaec3633293bf4e4e299
Scott <scotty.pat@gmail.com>
2017-06-28 16:06:06 -0600

    Changed moisture insert to support new comma delimited string

--- model.py
+++ model.py
@@ -1,20 +1,37 @@
 import web
 import json
+import datetime
 
 db = web.database(dbn='sqlite', db='pivotmanager.db')
 
-def storeMoistureData(strData, sampleTime):
-    jsonData = json.loads(strData)    
+def storeMoistureData(lstData, sampleTime):
+    dbValues = []    
+    
+    for strMoistureData in stkMoistureData:
+        jsonData = json.loads(strMoistureData)
+        
+        #Add date/time stamp, and copy of data string
+        jsonData["dateTime"] = sampleTime
+        jsonData["dataObj"] = jsonData
+        
+        dbValues.append(jsonData)
     
     try:
-        db.insert("moisture", sensorId=jsonData["sensorId"], depth1=jsonData["depth1"], depth2=jsonData["depth2"], depth3=jsonData["depth3"], depth4=jsonData["depth4"], vSys=jsonData["vSys"], soilTemp=jsonData["soilTemp"], dataObj=strData, dateTime=sampleTime)
+        #db.insert("moisture", sensorId=jsonData["sensorId"], depth1=jsonData["depth1"], depth2=jsonData["depth2"], depth3=jsonData["depth3"], depth4=jsonData["depth4"], vSys=jsonData["vSys"], soilTemp=jsonData["soilTemp"], dataObj=strData, dateTime=sampleTime)
+        db.multiple_insert("moisture", values=dbValues)
     
     except:
-        error = "Error Inserting Moisture Data: " + jsonData
+        error = "Error Inserting Moisture Data: " + dbValues
         print error
     
 def getMoistureUploadData():
     return db.select("dataLogs", where="uploded = 0 AND dataType='moisture'")
+
+def getMoistureLabels(weekDelta):
+    queryDate = datetime.datetime.now() - datetime.timedelta(weeks=weekDelta)
+    queryDate = queryDate.strftime('%Y/%m/%d %H:%M:%S')    
+    #return db.select("moisture", what='dateTime', order='dateTime')
+    return db.select("moisture", queryDate, what='dateTime', order='dateTime', where='dateTime > 2017/04/05 01:12:35')    
         
 def storeWeatherData(sData):
     db.insert("dataLogs", dataType="weather", dataObj=sData)
