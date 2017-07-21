b0e763ce5bdd7826990698fc67aeac8457db959f
Scott <scotty.pat@gmail.com>
2017-07-21 08:34:01 -0600

    Added web frotnend to graph moisture data

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import web
+import web  
 import json
 import datetime
 
@@ -39,7 +39,7 @@ def storeWeatherData(sData):
     
 def getWeatherUploadData():
     return db.select("dataLogs", where="uploded = 0 AND dataType='weather'")
-    
+
 def orgMoistureData(moistureData):
     chartData = {}
     chartData["depth1"] = []
@@ -47,11 +47,30 @@ def orgMoistureData(moistureData):
     chartData["depth3"] = []
     chartData["depth4"] = []
     for dataSample in moistureData:
-        singleSample = dict(dataSample)       
-        chartData["depth1"].append("{x: \"" + singleSample["dateTime"] + "\", y: " +  str(singleSample["depth1"]) + "}")        
-        chartData["depth2"].append("{x: \"" + singleSample["dateTime"] + "\", y: " +  str(singleSample["depth2"]) + "}")
-        chartData["depth3"].append("{x: \"" + singleSample["dateTime"] + "\", y: " +  str(singleSample["depth3"]) + "}")
-        chartData["depth4"].append("{x: \"" + singleSample["dateTime"] + "\", y: " +  str(singleSample["depth4"]) + "}")
-    
-    return chartData
+        singleSample = dict(dataSample)        
+        if singleSample["depth1"] > 0:
+            chartData["depth1"].append("{x: \"" + singleSample["dateTime"] + "\", y: " +  str(kpaMoistureData(singleSample["depth1"])) + "}")        
         
+        if singleSample["depth2"] > 0:
+            chartData["depth2"].append("{x: \"" + singleSample["dateTime"] + "\", y: " +  str(kpaMoistureData(singleSample["depth2"])) + "}")
+        
+        if singleSample["depth3"] > 0:
+            chartData["depth3"].append("{x: \"" + singleSample["dateTime"] + "\", y: " +  str(kpaMoistureData(singleSample["depth3"])) + "}")
+        
+        if singleSample["depth4"] > 0:
+            chartData["depth4"].append("{x: \"" + singleSample["dateTime"] + "\", y: " +  str(kpaMoistureData(singleSample["depth4"])) + "}")
+    
+    return chartData  
+    
+
+def kpaMoistureData(ohmReading):
+    ohmReading = ohmReading / 1000
+    soilTemp = 24
+    if ohmReading > 1:        
+        return (-3.213 * ohmReading - 4.093) / (1 - 0.009733 * ohmReading - 0.01205 * soilTemp)
+    
+    elif ohmReading > 8:
+         return -2.246 - 5.239 * ohmReading * (1 + 0.015 * (soilTemp -24)) - 0.06756 * ohmReading^2 * (1 + 0.018 * (soilTemp - 24))^2
+            
+    else:
+        return -20 * (ohmReading * (1 + 0.018 * (soilTemp - 24)) - 0.55)        
--- webservice.py
+++ webservice.py
@@ -1,28 +1,38 @@
 import web
+import model
 
 ## Url mappings
 
 urls = (
-    '/(.*)', 'Index',
-    '/weather', 'Weather',
-    '/moisture', 'Moisture'
+    "/", "Index",
+    "/moisture", "Moisture"
 )
 
 
 ### Templates
-render = web.template.render('templates/', base='basetemp')
+render = web.template.render('templates', base='basetemp')
 
 
 class Index:
 
     def GET(self):   
         
-        return render()
+        return render.index()
+               
+class Moisture:
 
+    def GET(self):
         
+        delta = web.input(weekDelta=None)
+        if delta.weekDelta == None:
+            weekOffset = 4
+        else:
+            weekOffset = int(delta.weekDelta)
+                           
+        moistureData = model.getMoistureData(weekOffset)        
+        moistureData = model.orgMoistureData(moistureData)        
+        return render.moisture(moistureData)
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
